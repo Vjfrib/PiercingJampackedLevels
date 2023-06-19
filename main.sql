c65908b2b02d13cc67476bc75f6073e5d36dec7e
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-06-19 17:03:57 +0000

    t3

--- main.sql
+++ main.sql
@@ -2,3 +2,28 @@ SELECT "Hello world!";
 SELECT "Hello world!";
 SELECT "Hello world!";
 
+public class BancoDados extends SQLiteOpenHelper {
+    public static final String NOME_BANCO = "banco.db";
+    public static final String NOME_TABELA = "usuario";
+    public static final int VERSAO_BANCO = 1;
+
+    public BancoDados(Context context) {
+        super(context, NOME_BANCO, null, VERSAO_BANCO);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase bd) {
+        bd.execSQL("create table if not exists " +
+                "usuario(_id integer primary key autoincrement, " +
+                "nome text not null," +
+                "login text not null," +
+                " senha text not null," +
+                " email text not null);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase bd, int oldVersion, int newVersion) {
+
+    }
+}
